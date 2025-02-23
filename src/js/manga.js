3c8c41dbd3ec03e483fcb09d3afad3e254a76d80
YanGabrielton <yangabrielton@gmail.com>
2025-02-23 20:10:40 -0300

    anime e manga api

--- src/js/manga.js
+++ src/js/manga.js
@@ -1,49 +1,48 @@
-// Função para buscar os animes da API do Jikan
-async function fetchAnimes() {
+// Função para buscar os mangás da API do Jikan
+async function fetchMangas() {
     try {
       // Fazendo a requisição à API do Jikan
       const response = await fetch('https://api.jikan.moe/v4/manga');
       
       // Verificando se a resposta foi bem-sucedida
       if (!response.ok) {
-        throw new Error('Erro ao buscar os animes');
+        throw new Error('Erro ao buscar os mangás');
       }
   
       // Convertendo a resposta para JSON
       const data = await response.json();
   
-      // Pegando o elemento da lista onde os animes serão exibidos
-      const animeList = document.getElementById('mainAnimes');
+      // Pegando o elemento da lista onde os mangás serão exibidos
+      const mangaList = document.getElementById('mainManga');
   
-      // Iterando sobre os animes e criando os elementos <li> para cada um
-      data.data.forEach(anime => {
+      // Iterando sobre os mangás e criando os elementos <li> para cada um
+      data.data.forEach(manga => {
         const listItem = document.createElement('li');
-        listItem.classList.add('anime-item'); // Classe para cada item da lista
+        listItem.classList.add('manga-item'); // Classe para cada item da lista
   
-        // Estrutura do anime sendo inserida dinamicamente
+        // Estrutura do mangá sendo inserida dinamicamente
         listItem.innerHTML = `
-        <div class="MainAnime">
-          <div class="anime">
-            <img src="${anime.images.jpg.image_url}" alt="${anime.title}" width="100">
-            <div class="anime-info">
-              <h3>${anime.title}</h3>
-              <span class="rating">${anime.score}</span> <!-- Adicionando a nota do anime -->
+        <div class="MainManga">
+          <div class="manga">
+            <img src="${manga.images.jpg.image_url}" alt="${manga.title}" width="100">
+            <div class="manga-info">
+              <h3>${manga.title}</h3>
+              <span class="rating">${manga.score}</span> <!-- Adicionando a nota do mangá -->
             </div>
             <div class="descricao">
-              <p>${anime.synopsis}</p> <!-- Descrição do anime -->
+              <p>${manga.synopsis}</p> <!-- Descrição do mangá -->
             </div>
           </div>
         </div>
         `;
   
-        // Adicionando o item à lista de animes
-        animeList.appendChild(listItem);
+        // Adicionando o item à lista de mangás
+        mangaList.appendChild(listItem);
       });
     } catch (error) {
       console.error('Erro ao carregar os dados: ', error);
     }
   }
   
-  // Chamando a função para buscar os animes quando a página carregar
-  fetchAnimes();
-  
+  // Chamando a função para buscar os mangás quando a página carregar
+  fetchMangas();
